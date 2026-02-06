e7fa917c9b2a4bb564b7219f93d1ce2077cbc75f
Chander <cbhuvan1@gmail.com>
2026-02-06 19:01:39 +0530

    Exercise 6 done

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,6 +1,21 @@
-const repeatString = function() {
+const repeatString = function (string, num) {
+  let finalString = "";
 
+  if (num < 0) {
+    return "ERROR";
+  } else {
+    for (let i = 0; i < num; i++) {
+      finalString = finalString + string;
+    }
+  }
+
+  return finalString;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+// ---------------------------- Logic ---------------------------- //
+// make a result string
+// run a loop for given number of times
+// each time the loop is run add the string to the result string
