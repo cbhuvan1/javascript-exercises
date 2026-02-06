e96343c8351a4037833ab69015470cff71d37baf
Chander <cbhuvan1@gmail.com>
2026-02-06 19:21:16 +0530

    Done exercise 07

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let reversedString = "";
+
+    let strArr = str.split("").reverse();
+
+    for (let item of strArr){
+        reversedString = reversedString + item;
+    }
+
+    return reversedString;
+    
 
 };
 
